e2c5fac85ea6e4470412f00dda0ea5b09c38b565
Evgeny Lupashko <lupashko.evgeny@gmail.com>
2024-12-29 11:47:16 +0000

    add go doc comments

--- math.go
+++ math.go
@@ -1,5 +1,11 @@
+// Package math provides elementary math functions.
 package math
 
+// Add returns a sum of its two integer parameters.
+//
+// More information on addition can be found at [Math is Fun].
+//
+// [Math is Fun]: https://www.mathsisfun.com/numbers/addition.html
 func Add(a, b int) int {
 	return a + b
 }
